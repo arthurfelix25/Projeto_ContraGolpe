cb30072ea6d26fe05533c155e7b95600f50e8e92
Arthur <arthur.felixa25@gmail.com>
2025-11-09 12:42:02 -0300

    feat: new button, login compaines

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return(
-        <nav className="bg-[#f2f2f2] text-[#00008B] border-b-[1px] border-b-[#d2d2d2] flex justify-between items-stretch gap-8 py-0 px-4 font-bold text-[22px]">
+    <nav className="bg-[#f2f2f2] text-[#00008B] border-b-[1px] border-b-[#d2d2d2] flex justify-between items-center gap-8 py-0 px-4 font-bold text-[22px]">
             <Link to="/" className="text-[2rem]">Início</Link>
             <ul className="m-0 p-0 list-none flex gap-4">
                 <li>
@@ -13,6 +13,9 @@ function Navbar() {
                 </li>
                 <li>
                     <Link to="/empresas" className="flex no-underline h-full text-inherit items-center p-1 hover:bg-[#d2d2d2]">Empresas mais Usadas</Link>
+                </li>
+                <li>
+                    <Link to="/rempresas" className="rounded-md bg-[#0b3d91] px-3 py-1.5 text-white font-semibold text-sm hover:bg-[#0e56bd]">Login Empresa</Link>
                 </li>
             </ul>
         </nav>
