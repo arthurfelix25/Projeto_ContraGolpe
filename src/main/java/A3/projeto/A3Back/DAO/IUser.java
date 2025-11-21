b1d6b9c021747dd9627bb195fc8dbc2ea808c319
Lucas Abel <161757291+MTsunaM@users.noreply.github.com>
2025-11-20 23:26:10 -0300

    Delete src directory

--- src/main/java/A3/projeto/A3Back/DAO/IUser.java
+++
@@ -1,8 +0,0 @@
-package A3.projeto.A3Back.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import A3.projeto.A3Back.model.UserModel;
-
-public interface IUser extends CrudRepository<UserModel, Integer> {
-}
