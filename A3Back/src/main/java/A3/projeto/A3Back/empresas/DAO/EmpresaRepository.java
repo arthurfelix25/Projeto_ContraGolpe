3df1ab393ed0af38aa1527ac814c18025586725d
Lucas Abel <161757291+MTsunaM@users.noreply.github.com>
2025-12-01 23:52:35 -0300

    Delete A3Back directory

--- A3Back/src/main/java/A3/projeto/A3Back/empresas/DAO/EmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package A3.projeto.A3Back.empresas.DAO;
-
-import A3.projeto.A3Back.empresas.model.Empresa;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmpresaRepository extends CrudRepository<Empresa, Integer> {
-    boolean existsByUsuario(String usuario);
-    boolean existsByCnpj(String cnpj);
-}
