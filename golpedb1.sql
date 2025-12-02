8d11441ab1d72e7402314c9b3fccb29509de31b0
Lucas Abel <161757291+MTsunaM@users.noreply.github.com>
2025-12-02 08:35:05 -0300

    Update golpedb1.sql

--- golpedb1.sql
+++ golpedb1.sql
@@ -2,28 +2,18 @@ CREATE DATABASE IF NOT EXISTS golpe_db_simple
   DEFAULT CHARACTER SET utf8mb4;
 USE golpe_db_simple;
 
-CREATE TABLE IF NOT EXISTS golpes (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE `golpes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(120) NOT NULL,
+  `cidade` VARCHAR(120) NOT NULL,
+  `cpf` VARCHAR(14) NOT NULL,
+  `meio_contato` ENUM('Telefone','WhatsApp','Email','SMS','Outros') NOT NULL,
+  `descricao` TEXT NOT NULL,
+  `email_telefone` VARCHAR(120) NOT NULL,
+  `empresa_id` INT UNSIGNED DEFAULT NULL,
+  `empresa` VARCHAR(120) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_empresa_id` (`empresa_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-  nome VARCHAR(120) NOT NULL, 
-  cidade VARCHAR(120) NOT NULL, 
-
-  meio_contato ENUM(
-    'Telefone',
-    'WhatsApp',
-    'Email',
-    'SMS',
-    'Outros'
-  ) NOT NULL,
-
-  empresa VARCHAR(160) NULL,   
-
-  cpf VARCHAR(14) NOT NULL,
-
-  descricao TEXT NOT NULL,   
-
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-  
-) 
-
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
